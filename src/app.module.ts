496d9d82f537f43dbd2676b9954533eaf02d428b
IbrahimMohamed9 <ibrahemmohamedb@gmail.com>
2025-02-07 22:46:35 +0100

    config db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
+import { UserLoginLocationModule } from './user-login-location/user-login-location.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, UsersModule, UserLoginLocationModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
