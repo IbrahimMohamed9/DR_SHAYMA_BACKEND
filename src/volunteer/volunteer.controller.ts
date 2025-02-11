ad5a8cd613a656a85b6dc2fcb813445ad80292ea
IbrahimMohamed9 <ibrahemmohamedb@gmail.com>
2025-02-11 22:02:00 +0100

    add guards for the volunteers

--- src/volunteer/volunteer.controller.ts
+++ src/volunteer/volunteer.controller.ts
@@ -6,19 +6,26 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { VolunteerService } from './volunteer.service';
 import { CreateVolunteerDto } from './dto/create-volunteer.dto';
 import { UpdateVolunteerDto } from './dto/update-volunteer.dto';
 import { Volunteer } from './entities/volunteer.entity';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { OnlyAdminGuard } from 'src/auth/guards/only-admin.guard';
 
 @Controller('volunteer')
 export class VolunteerController {
   constructor(private readonly volunteerService: VolunteerService) {}
 
-  @ApiOperation({ summary: 'Create new volunteer ' })
+  @ApiOperation({ summary: 'Create new volunteer' })
   @ApiResponse({ status: 201, type: Volunteer })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'), OnlyAdminGuard)
   @Post()
   create(@Body() createVolunteerDto: CreateVolunteerDto) {
     return this.volunteerService.create(createVolunteerDto);
@@ -38,6 +45,11 @@ export class VolunteerController {
     return this.volunteerService.findOne(+id);
   }
 
+  @ApiResponse({ status: 200, type: Volunteer })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'), OnlyAdminGuard)
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -46,6 +58,10 @@ export class VolunteerController {
     return this.volunteerService.update(+id, updateVolunteerDto);
   }
 
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'), OnlyAdminGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.volunteerService.remove(+id);
