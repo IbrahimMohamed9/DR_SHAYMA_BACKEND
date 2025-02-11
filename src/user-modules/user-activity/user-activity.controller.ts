a2ade40b7191cd7d6a6339faba304fdfdc72c613
IbrahimMohamed9 <ibrahemmohamedb@gmail.com>
2025-02-11 21:59:54 +0100

    add guards for user activiry

--- src/user-modules/user-activity/user-activity.controller.ts
+++ src/user-modules/user-activity/user-activity.controller.ts
@@ -1,53 +1,47 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, UseGuards } from '@nestjs/common';
 import { UserActivityService } from './user-activity.service';
 import { CreateUserActivityDto } from './dto/create-user-activity.dto';
-import { UpdateUserActivityDto } from './dto/update-user-activity.dto';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { UserActivity } from './entities/user-activity.entity';
+import { AuthGuard } from '@nestjs/passport';
+import { OnlyMeGuard } from 'src/auth/guards/only-me.guard';
+import { MeAndAdminGuard } from 'src/auth/guards/me-and-admin.guard';
+import { OnlyAdminGuard } from 'src/auth/guards/only-admin.guard';
 
 @Controller('user-activity')
 export class UserActivityController {
   constructor(private readonly userActivityService: UserActivityService) {}
 
-  @Post()
   @ApiOperation({ summary: 'Create user activity' })
   @ApiResponse({ status: 201, type: UserActivity })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'), OnlyMeGuard)
+  @Post()
   create(@Body() createUserActivityDto: CreateUserActivityDto) {
     return this.userActivityService.create(createUserActivityDto);
   }
 
-  @Get()
   @ApiOperation({ summary: 'Get all user activities' })
   @ApiResponse({ status: 200, type: UserActivity, isArray: true })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'), MeAndAdminGuard)
+  @Get()
   findAll() {
     return this.userActivityService.findAll();
   }
 
-  @Get(':id')
   @ApiOperation({ summary: 'Get user activity by id' })
   @ApiResponse({ status: 200, type: UserActivity })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'), OnlyAdminGuard)
+  @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userActivityService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateUserActivityDto: UpdateUserActivityDto,
-  ) {
-    return this.userActivityService.update(+id, updateUserActivityDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userActivityService.remove(+id);
   }
 }
