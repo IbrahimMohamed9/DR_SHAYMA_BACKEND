29fb06ba07366a678791774454d98ebe72f64942
IbrahimMohamed9 <ibrahemmohamedb@gmail.com>
2025-02-11 22:18:15 +0100

    implements feedback endpoints

--- src/feedback-modules/feedback/feedback.controller.ts
+++ src/feedback-modules/feedback/feedback.controller.ts
@@ -6,12 +6,17 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { FeedbackService } from './feedback.service';
 import { CreateFeedbackDto } from './dto/create-feedback.dto';
 import { UpdateFeedbackDto } from './dto/update-feedback.dto';
 import { Feedback } from './entities/feedback.entity';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { OnlyAdminGuard } from 'src/auth/guards/only-admin.guard';
 
 @Controller('feedback')
 export class FeedbackController {
@@ -19,21 +24,38 @@ export class FeedbackController {
 
   @ApiOperation({ summary: 'Create new feedback' })
   @ApiResponse({ status: 201, type: Feedback })
+  @UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
   @Post()
   create(@Body() createFeedbackDto: CreateFeedbackDto) {
     return this.feedbackService.create(createFeedbackDto);
   }
 
+  @ApiOperation({ summary: 'Create new feedback' })
+  @ApiResponse({ status: 200, type: [CreateFeedbackDto] })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'), OnlyAdminGuard)
   @Get()
   findAll() {
     return this.feedbackService.findAll();
   }
 
+  @ApiResponse({ status: 200, type: CreateFeedbackDto })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'), OnlyAdminGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.feedbackService.findOne(+id);
   }
 
+  @ApiResponse({ status: 200, type: UpdateFeedbackDto })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'), OnlyAdminGuard)
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -42,6 +64,11 @@ export class FeedbackController {
     return this.feedbackService.update(+id, updateFeedbackDto);
   }
 
+  @ApiResponse({ status: 204, description: 'Deleted successfully' })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'), OnlyAdminGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.feedbackService.remove(+id);
