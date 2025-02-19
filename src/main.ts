5dbd856061e901a74a0ad1b5d9cda7e331e47fbb
IbrahimMohamed9 <ibrahemmohamedb@gmail.com>
2025-02-19 01:09:18 +0100

    refactor(main): enable CORS with specific origin and methods for improved security

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,14 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
